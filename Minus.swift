4f259ffcb03c905215c3956b052d7f42884b81d5
Hamed Hosseini <hamed8080@gmail.com>
2021-04-12 08:03:49 +0430

    add minus

---
+++ Minus.swift
@@ -0,0 +1,14 @@
+//
+//  Minus.swift
+//  testCocopods
+//
+//  Created by Hamed Hosseini on 4/12/21.
+//
+
+import Foundation
+public class Minus {
+    
+    public class func getMinus(number1:Int , number2:Int )->Int{
+       return number1 - number2
+    }
+}
